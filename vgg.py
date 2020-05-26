dc355c24dedac15c674f586186155f9b958b01e3
Andre Daprato <andre.daprato5@gmail.com>
2020-05-26 16:17:57 -0400

    add files for saved weights from pytorch

---
+++ vgg.py
@@ -0,0 +1,14 @@
+from torchvision import models, datasets
+import torch
+
+
+
+model = models.vgg16(pretrained=True)
+print(model.features)
+example = torch.rand(1,3,224,224)
+
+nth = lambda x, y: model.features[x](y)
+
+torch.cuda.is_available
+# script = torch.jit.trace(model,example)
+# torch.jit.save(script, "vgg16.pt")
